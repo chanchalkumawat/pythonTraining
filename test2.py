254ebc2644879b59b75048c7d65d903dd5dcd7f7
sarangmulewa <sarangmuleva.1@gmail.com>
2017-06-07 07:18:16 +0530

    third day of training

---
+++ test2.py
@@ -0,0 +1,79 @@
+#....................... String Formating.....................
+
+# name = "Sarang"
+# name_update = 10
+# print (name + str(name_update))
+
+# my_rollno = "50"
+# int_rollno = int(my_rollno)
+
+# print ("Type of my_rollno is: " + str(type(my_rollno)), " Type of int_rollno is: "+ str(type(int_rollno)))
+# print ("The string is %s" %my_rollno)
+
+#.......................Imports in Python......................
+
+# import time
+# import datetime
+# from time import ctime
+# from django import test
+
+# print (ctime())
+# print (datetime.datetime.now())
+
+#.......................Conditions and statements.................
+
+# def test():
+# 	testvar = 10
+# 	a = 5
+
+# 	if a == 5:
+# 		print("Hello")
+
+# 		if testvar == 10:
+# 			print("Hello hi")
+
+# 		else:
+# 			print("Bye Bye")
+
+# ...Range....
+
+# for i in range(4,10):
+#  	print(i)
+
+#.....................List & Dictionaries.........................
+
+# test_list = ["Sarang","Mulewa","Hello",1,2,3,4,5,True,False]
+
+# my_dict = {"name": ['consultadd',' tixdo'], 'location': 'India', 'employees': 'Two_Hundred'}
+
+# print(test_list[8])
+
+# for data in my_dict:
+# 	for i in range(1):
+		
+# 		print(data)
+
+
+# for k , v in my_dict.iteritems():
+# 	print v
+
+# print test_list[1:2]
+# List_name[lower range : Higher range] 
+
+
+#..........................Python Tuple......................
+
+# a = (1,2,3)
+# print (a[0])
+
+#..........................file handling......................
+
+# one_file = open("test.txt", 'w')
+# one_file.write("Hello Sarang\n")
+# one_file.write("Welcome")
+# one_file.close()
+
+# import json
+# one_file = open("test.json",'w')
+# one_file.write(json.dumps(my_dict))
+# one_file.close()
